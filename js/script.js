4b572543ecad271edb7b8c9716d0085396d3b839
jhonmanzan <126723438+jhonmanzan@users.noreply.github.com>
2024-05-24 17:26:00 -0300

    site jb atualizado

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function openModal(imageSrc) {
+  var modal = document.getElementById("modal");
+  var modalImage = document.getElementById("modal-image");
+
+  modal.style.display = "block";
+  modalImage.src = imageSrc;
+}
+
+function closeModal() {
+  var modal = document.getElementById("modal");
+  modal.style.display = "none";
+}
